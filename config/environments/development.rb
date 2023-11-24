5808f323bd9ad16f13cffb471509afeefb9b07d1
Fragarias <148366640+Fragarias@users.noreply.github.com>
2023-11-24 15:49:10 +0000

    [Finish] 4-1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "a2c2b18df199422db6e203d92e81471e.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  config.active_job.queue_adapter = :inline
+  config.hosts.clear
 end
