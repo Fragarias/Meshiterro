75cfe158a31d9e4250c1ee615cbef774a7674ee3
Fragarias <148366640+Fragarias@users.noreply.github.com>
2023-11-27 15:47:32 +0000

    [Finish] 4-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'homes/about', as: 'about'
-  root to: "homes#top"
 
+  devise_for :users
+
+  # get 'homes/top'
+  root to: "homes#top"
+  get 'homes/about', as: 'about'
+  
+  
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
+  # get 'users/show'
+  # get 'users/edit'
+  resources :users, only: [:show, :edit]
 
 end
